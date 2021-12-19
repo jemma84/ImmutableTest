ac1b1b3db39d6f8169b95afd80ebaa9bf6343146
jemma84 <40545469+jemma84@users.noreply.github.com>
2021-12-19 14:07:16 +0300

    ImmutableTest

--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 import Immutable from "immutable";
+const { Map } = require("immutable");
 
 let basket = Immutable.Map({ milk: "yes", flour: "no" });
 
@@ -8,7 +9,18 @@ console.log(basket.toJS());
 let basketDeep = Immutable.Map({
   fruits: Immutable.Map({ oranges: "no" }),
   flour: "no"
-});
+}); //all js-objects should be noverted to Map(see below)
 
 basketDeep = basketDeep.setIn(["fruits", "oranges"], "yes");
 console.log(basketDeep.toJS());
+
+//easiest way
+const originalMap = Immutable.fromJS({
+  subObject: {
+    subKey: "subvalue",
+    subSubObject: {
+      subSubKey: "subSubValue"
+    }
+  }
+}); //Deeply converts plain JS objects and arrays to Immutable Maps and Lists.
+originalMap.setIn(["subObject", "subKey"], "ha ha!");
